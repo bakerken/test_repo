50c4e390c57bdb25fb81138e5371cb78268e4396
Ken Baker <ken@generalbioinformatics.com>
2017-10-27 15:19:47 +0100

    Added copyright tag.

--- test.pl
+++ test.pl
@@ -1,4 +1,10 @@
 #!/usr/bin/env perl
 
+#############################################################################
+# Copyright (C) 2017 General Bioinformatics Limited.                        #
+# Distributed under the GNU GPL v2. For full terms see the file LICENSE.    #
+#############################################################################
+
+
 print "This script does very little\n";
 
